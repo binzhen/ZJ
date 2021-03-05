cbc5d5cda0690c5ccc37f558ba4087b791089842
binzhen <80035394+binzhen@users.noreply.github.com>
2021-03-05 12:55:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 s=requests.Session()
-headers={'cookie':'stuid=27011E8244D423A5&stuxm=AD3C218B1A157BE6&stubj=7285B8596DEF819F961F13D942957AE819DBB0DE49999A22	'}
+headers={'cookie':'stuinfo=stuid=27011E8244D423A5&stuxm=AD3C218B1A157BE6&stubj=7285B8596DEF819F961F13D942957AE819DBB0DE49999A22'}
 r=s.post("https://fyns.eduw.cn/stu/dk9.aspx",headers=headers)
 data={'Button2':' 当前情况和上次一样，系统直接帮我打卡' , '__VIEWSTATEGENERATOR': 'BEBF4070' , '__VIEWSTATE': '/wEPDwUKLTk3MjYyNTU4NGRkbQwAF57GOqyjzXoXKqZTAftYIH3j/SOdUxlgAK6g7Oc='}
 k=s.post('https://fyns.eduw.cn/stu/dk9.aspx',data=data,headers=headers)
